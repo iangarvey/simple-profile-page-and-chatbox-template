e901108d7a43ea8f24b96a334f8e4df21f003d8d
Ian Garvey <ianarthurgarvey@gmail.com>
2025-09-03 13:43:46 +0000

    the register button now changes to a 'my profile' button and links to /myprofile

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -52,9 +52,15 @@ export const Navbar = () => {
 						<button className="btn btn-success">Login</button>
 					</Link>
 				)}
-				<Link to="/register">
-					<button className="btn btn-primary">Register</button>
-				</Link>
+				{loggedIn ? (
+					<Link to="/myprofile">
+						<button className="btn btn-info">My Profile</button>
+					</Link>
+				) : (
+					<Link to="/register">
+						<button className="btn btn-primary">Register</button>
+					</Link>
+				)}
 			</div>
 		</nav>
 	);
